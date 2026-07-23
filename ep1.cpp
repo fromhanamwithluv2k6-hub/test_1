af805f4a29d62b6593ca7ababbe4d5261c585621
di3_y0ung <fromhanamwithluv2k6@gmail.com>
2026-07-24 06:20:31 +0700

    sua ep1

--- ep1.cpp
+++ ep1.cpp
@@ -1,18 +1,38 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
+bool div_7(string s)
+{
+    int m = 0;
+    for(int i = 0 ; i <s.size() ; i++)
+    {
+        m = (m * 10 + (s[i]-'0')) % 7;
+    }
+    if(m == 0) return 1;
+    return 0;
+}
+bool div_13(string s)
+{
+    int m = 0;
+    for(int i = 0 ; i <s.size() ; i++)
+    {
+        m = (m * 10 + (s[i]-'0')) % 13;
+    }
+    if(m == 0) return 1;
+    return 0;
+}
+void solve()
+{
+    string s; cin>>s;
+    if(div_7(s) && div_13(s)) cout<<"Both";
+    else if(div_7(s) && !div_13(s)) cout<<"Div 7";
+    else if(!div_7(s) && div_13(s)) cout<<"Div 13";
+    else cout<<"None";
+    cout<<"\n";
+}
 int main()
 {
-    int n; cin>>n;
-    vector<int> v(n);
-    for(int i = 0 ; i<n  ; i++)
-    {
-        cin>>v[i];
-    }
-   for(auto i : v)
-   {
-    cout<<i<<" ";
-   }
-   cout<<endl;
+    int t; cin>>t;
+    while(t--)
+        solve();
+    return 0;
 }
