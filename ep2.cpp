1588a187a68e064dbd2bfe25a68e4661223a3c60
di3_y0ung <fromhanamwithluv2k6@gmail.com>
2026-07-24 10:32:50 +0700

    add ep2

--- ep2.cpp
+++ ep2.cpp
@@ -1,11 +1,41 @@
 #include<bits/stdc++.h>
+#include<cmath>
 using namespace std;
+vector<long long> pow3(40),pow5(26);
+void init()
+{
+    for(int i = 0 ; i<40 ; i++)
+    {
+        pow3[i] = (long long)pow(3,i);
+    }
+    for(int i = 0 ; i<26 ; i++)
+    {
+        pow5[i] = (long long)pow(5,i);
+    }
+}
+void solve()
+{
+    long long n; cin>>n;
+    for(int i = 0 ; i<40 ; i++)
+    {
+        for(int j = 0 ; j <26 ; j++)
+        {
+            if(pow3[i] + pow5[j] == n)
+            {
+                cout<<i<<" "<<j<<endl;
+                return;
+            }
+        }
+    }
+    cout<<-1<<endl;
+}
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    init();
     int t; cin>>t;
     while(t--)
-    {
-        cout<<"Hello World\n";
-    }
+        solve();
     return 0;
 }
