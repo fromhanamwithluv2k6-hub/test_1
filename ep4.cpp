7e506270f1e757573d5ae99cd24248d2d983b195
di3_y0ung <fromhanamwithluv2k6@gmail.com>
2026-07-28 13:51:28 +0700

    add ep4

---
+++ ep4.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MOD = 1e9 + 7;
+bool isprime(int n)
+{
+    if(n < 2) return 0;
+    for(int i = 2 ; i<=sqrt(n) ; i++)
+    {
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+int lengdre(int n,int p)
+{
+    int res = 0;
+    for(int i = p ; i<=n ; i *= p)
+        res += n/i;
+    return res;
+}
+int main()
+{
+    int n; cin>>n;
+    long long res = 1;
+    for(int i = 2 ; i<=n ; i++)
+    {
+        if(isprime(i))
+        {
+            res *= (lengdre(n,i) + 1);
+            res %= MOD;
+        }
+    }
+    cout<<res<<endl;
+}
