cbaf7d60d8a141ba4e783fe94351c0d0295b2720
di3_y0ung <fromhanamwithluv2k6@gmail.com>
2026-07-28 14:02:41 +0700

    add ep5

--- ep5.cpp
+++ ep5.cpp
@@ -4,6 +4,8 @@ using namespace std;
 const int MOD = 1e9 + 7;
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n,k; cin>>n>>k;
     map<int,int> mp;
     while(k--)
